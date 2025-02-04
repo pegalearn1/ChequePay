3973943ac7852e573ba306e6907a4a1c874b640d
pegalearn1 <pegalearn1@gmail.com>
2025-02-04 19:44:02 +0530

    updated report module

--- chq_pay/views/report_views.py
+++ chq_pay/views/report_views.py
@@ -10,46 +10,32 @@ User = get_user_model()
 @login_required
 def reports(request):
     cheques = ChequeIssue.objects.all().filter(Q(company__is_selected=True)).order_by('-issue_cheque_date')
-    templates = ChequeTemplate.objects.all()
-    banks = Banks.objects.all()
+    banks = ChequeIssue.objects.values_list('issue_bank__id','issue_bank__bank_name_e').distinct()
     accounts = ChequeIssue.objects.values_list('issue_accountnum', 'issue_bank__bank_name_e').distinct()
-    payees = Payee.objects.all()
-    chq_txts = ChequeText.objects.all()
+    payees = ChequeIssue.objects.select_related('issue_payee').distinct()
 
-    # print("accountsss - ", accounts)
-
-    # Apply filters if provided in the request
-    search = request.GET.get('search', '')
-    if search:
-        cheques = cheques.filter(
-            Q(issue_accountnum__icontains=search) |
-            Q(issue_payee__payee_name__icontains=search) |
-            Q(issue_cheque_no__icontains=search) |
-             Q(issue_template__name__icontains=search)
-        )
-
-     # Fetch filter parameters for Bank And Currency
-    selected_bank = request.GET.get('bank')
-    selected_currency = request.GET.get('currency')
     
+    # Fetch filter parameters for Bank and Approval
+    selected_bank = request.POST.get('bank', '')
+    selected_approval = request.POST.get('approval', '')
+    start_date = request.POST.get('start_date', '')
+    end_date = request.POST.get('end_date', '')
+
+    
+    # Filter by bank
     if selected_bank:
         cheques = cheques.filter(issue_bank__id=selected_bank)
-    if selected_currency:
-        cheques = cheques.filter(issue_currency__id=selected_currency)
 
     # Filter by approval status
-    approval = request.GET.get('approval', '')
-    if approval:
-        if approval == 'approved':
+    if selected_approval:
+        if selected_approval == 'approved':
             cheques = cheques.filter(issue_is_approved=True)
-        elif approval == 'pending':
+        elif selected_approval == 'pending':
             cheques = cheques.filter(issue_is_approved=None)
-        elif approval == 'rejected':
+        elif selected_approval == 'rejected':
             cheques = cheques.filter(issue_is_approved=False)
 
     # Filter by cheque date range
-    start_date = request.GET.get('start_date', '')
-    end_date = request.GET.get('end_date', '')
     if start_date and end_date:
         cheques = cheques.filter(issue_cheque_date__range=[start_date, end_date])
 
@@ -62,17 +48,27 @@ def reports(request):
         except User.DoesNotExist:
             chq.user_first_name = None
 
+    
+    # If POST request, get selected checkboxes
+    if request.method == 'POST':
+        selected_payees = request.POST.getlist('selected_payees')
+        selected_accounts = request.POST.getlist('selected_accounts')
+
+        print("payeeesss -- ", selected_payees)
+        print("acountsss -- ", selected_accounts)
+        print("getbanks - ", selected_bank)
+        print("getapproval - ", selected_approval)
+        print("getstdate - ", start_date)
+        print("geteddate - ", end_date)
 
     # Context for the template
     context = {
         'cheques': cheques,
-        'templates': templates,
         'banks': banks,
         'accounts': accounts,
         'payees': payees,
-        'chq_txts': chq_txts,
-        'search': search,
-        'approval': approval,
+        'sel_bank':int(selected_bank) if selected_bank else None,
+        'approval': selected_approval,
         'start_date': start_date,
         'end_date': end_date,
     }
