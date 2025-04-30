03caa432b2229147b3561ea51fd9a176268e4a82
pegalearn1 <pegalearn1@gmail.com>
2025-04-30 12:39:43 +0530

    pycache

--- ChequePay/db_config.py
+++ ChequePay/db_config.py
@@ -1,35 +1,7 @@
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {    
-    '3132': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3132.sqlite3',
-    },
-    '3161': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3161.sqlite3',
-    },
-    '3151': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3151.sqlite3',
-    },
-    '3122': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3122.sqlite3',
-    },
-    '3156': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3156.sqlite3',
-    },
-    '3154': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3154.sqlite3',
-    },
-    '3117': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3117.sqlite3',
-    },
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'chqpaydb.sqlite3',
