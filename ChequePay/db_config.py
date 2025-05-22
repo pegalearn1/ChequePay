dcdb3c0780b31ae5046b0058f2b63425fc880fa9
pegalearn1 <pegalearn1@gmail.com>
2025-05-22 16:23:31 +0530

    updated csrf trusted list

--- ChequePay/db_config.py
+++ ChequePay/db_config.py
@@ -2,10 +2,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
-    '3117': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'chqpaydb_3117.sqlite3',
-    },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'chqpaydb.sqlite3',
